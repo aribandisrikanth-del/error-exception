2910870d3c0c0580d123a4698312eb5c2e249ab7
aribandisrikanth-del <aribandi.srikanth@gmail.com>
2026-02-26 20:17:23 -0500

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,6 @@
+list1=[]
+list2=[1,2,3,4,5,6,7]
+list3=[30/5]
+list4=[3,6,9,12]
+list4=list4[::-1]
+print("list number 1 is",list1,"list number 2 is",list2,"list number 3 is",list3,"list number 4 is",list4)
